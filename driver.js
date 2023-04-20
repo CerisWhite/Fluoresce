51a70483f3316c9be0ce4a3a83f780580e615f4a
Ceris White <60857285+CerisWhite@users.noreply.github.com>
2023-04-20 00:36:08 -0500

    (partially untested) driver.js
    Using Fluoresce is easy. Just `require` driver.js and have fun.

---
+++ driver.js
@@ -0,0 +1,75 @@
+// Fluoresce DB "driver"
+const net = require('net');
+
+function Create(Database) {
+	const socket = net.connect(4781, "127.0.0.1");
+	socket.write(JSON.stringify({'type': "create", 'destination': Database, 'userid': 0}));
+	socket.on("data", (data) => {
+		Response = JSON.parse(data);
+		socket.destroy();
+		return Response;
+	});
+}
+
+function Delete(Database, UserID) {
+	const socket = net.connect(4781, "127.0.0.1");
+	if (typeof UserID == Number || typeof UserID == String) {
+		socket.write(JSON.stringify({'type': "delete", 'destination': Database, 'userid': UserID}));
+		socket.on("data", (data) => {
+			Response = JSON.parse(data);
+			socket.destroy();
+			return Response;
+		});
+	}
+	else {
+		socket.write(JSON.stringify({'type': "destroy", 'destination': Database, 'userid': 0}));
+		socket.on("data", (data) => {
+			Response = JSON.parse(data);
+			socket.destroy();
+			return Response;
+		});
+	}
+}
+
+function Read(Database, UserID) {
+	const socket = net.connect(4781, "127.0.0.1");
+	socket.write(JSON.stringify({'type': "read", 'destination': Database, 'userid': UserID }));
+	socket.on("data", (data) => {
+		Response = JSON.parse(data);
+		socket.destroy();
+		return Response;
+	});
+}
+
+function Write(Database, UserID, Data) {
+	const socket = net.connect(4781, "127.0.0.1");
+	socket.write(JSON.stringify({'type': "write", 'destination': Database, 'userid': UserID, 'data': Data }));
+	socket.on("data", (data) => {
+		Response = JSON.parse(data);
+		socket.destroy();
+		return Response;
+	});
+}
+
+function Exists(Database, UserID) {
+	const socket = net.connect(4781, "127.0.0.1");
+	if (typeof UserID == Number || typeof UserID == String) {
+		socket.write(JSON.stringify({'type': "exists", 'destination': Database, 'userid': UserID }));
+		socket.on("data", (data) => {
+			Response = JSON.parse(data);
+			socket.destroy();
+			return Response['exists'];
+		});
+	}
+	else {
+		socket.write(JSON.stringify({'type': "exists", 'destination': Database, 'userid': 0 }));
+		socket.on("data", (data) => {
+			Response = JSON.parse(data);
+			socket.destroy();
+			return Response['exists'];
+		});
+	}
+	return false;
+}
+
+module.exports = { Create, Delete, Read, Write, Exists };
