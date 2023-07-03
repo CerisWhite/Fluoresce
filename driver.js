231f4639d478ba827b479e480d93d9034bcc857b
Ceris White <60857285+CerisWhite@users.noreply.github.com>
2023-07-02 19:37:35 -0500

    promisified driver.js

--- driver.js
+++ driver.js
@@ -2,84 +2,95 @@
 const net = require('net');
 
 function Create(Database) {
-	const socket = net.connect(4781, "127.0.0.1");
-	socket.write(JSON.stringify({'type': "create", 'destination': Database, 'userid': 0}));
-	socket.on("data", (data) => {
-		Response = JSON.parse(data);
-		socket.destroy();
-		return Response;
+	return new Promise((resolve, reject) => {
+		const socket = net.connect(4781, "127.0.0.1");
+		socket.write(JSON.stringify({'type': "create", 'destination': Database, 'userid': 0}));
+		socket.on("data", (data) => {
+			Response = JSON.parse(data);
+			socket.destroy();
+			resolve(Response);
+		});
 	});
 }
 
 function Delete(Database, UserID) {
-	const socket = net.connect(4781, "127.0.0.1");
-	if (typeof UserID == Number || typeof UserID == String) {
-		socket.write(JSON.stringify({'type': "delete", 'destination': Database, 'userid': UserID}));
-		socket.on("data", (data) => {
-			Response = JSON.parse(data);
-			socket.destroy();
-			return Response;
-		});
-	}
-	else {
-		socket.write(JSON.stringify({'type': "destroy", 'destination': Database, 'userid': 0}));
-		socket.on("data", (data) => {
-			Response = JSON.parse(data);
-			socket.destroy();
-			return Response;
-		});
-	}
+	return new Promise((resolve, reject) => {
+		const socket = net.connect(4781, "127.0.0.1");
+		if (typeof UserID == Number || typeof UserID == String) {
+			socket.write(JSON.stringify({'type': "delete", 'destination': Database, 'userid': UserID}));
+			socket.on("data", (data) => {
+				Response = JSON.parse(data);
+				socket.destroy();
+				resolve(Response);
+			});
+		}
+		else {
+			socket.write(JSON.stringify({'type': "destroy", 'destination': Database, 'userid': 0}));
+			socket.on("data", (data) => {
+				Response = JSON.parse(data);
+				socket.destroy();
+				resolve(Response);
+			});
+		}
+	});
 }
 
 function Read(Database, UserID) {
-	const socket = net.connect(4781, "127.0.0.1");
-	socket.write(JSON.stringify({'type': "read", 'destination': Database, 'userid': UserID }));
-	socket.on("data", (data) => {
-		Response = JSON.parse(data);
-		socket.destroy();
-		return Response;
+	return new Promise((resolve, reject) => {
+		const socket = net.connect(4781, "127.0.0.1");
+		socket.write(JSON.stringify({'type': "read", 'destination': Database, 'userid': UserID }));
+		socket.on("data", (data) => {
+			Response = JSON.parse(data);
+			socket.destroy();
+			resolve(Response);
+		});
 	});
 }
 
 function Write(Database, UserID, Data) {
-	if (UserID == 0) { return false; }
-	const socket = net.connect(4781, "127.0.0.1");
-	socket.write(JSON.stringify({'type': "write", 'destination': Database, 'userid': UserID, 'data': Data }));
-	socket.on("data", (data) => {
-		Response = JSON.parse(data);
-		socket.destroy();
-		return Response;
+	return new Promise((resolve, reject) => {
+		if (UserID == 0) { return false; }
+		const socket = net.connect(4781, "127.0.0.1");
+		socket.write(JSON.stringify({'type': "write", 'destination': Database, 'userid': UserID, 'data': Data }));
+		socket.on("data", (data) => {
+			Response = JSON.parse(data);
+			socket.destroy();
+			resolve(Response);
+		});
 	});
 }
 
 function Exists(Database, UserID) {
-	const socket = net.connect(4781, "127.0.0.1");
-	if (typeof UserID == Number || typeof UserID == String) {
-		socket.write(JSON.stringify({'type': "exists", 'destination': Database, 'userid': UserID }));
-		socket.on("data", (data) => {
-			Response = JSON.parse(data);
-			socket.destroy();
-			return Response['exists'];
-		});
-	}
-	else {
-		socket.write(JSON.stringify({'type': "exists", 'destination': Database, 'userid': 0 }));
-		socket.on("data", (data) => {
-			Response = JSON.parse(data);
-			socket.destroy();
-			return Response['exists'];
-		});
-	}
-	return false;
+	return new Promise((resolve, reject) => {
+		const socket = net.connect(4781, "127.0.0.1");
+		if (typeof UserID == Number || typeof UserID == String) {
+			socket.write(JSON.stringify({'type': "exists", 'destination': Database, 'userid': UserID }));
+			socket.on("data", (data) => {
+				const Response = JSON.parse(data);
+				socket.destroy();
+				resolve(Response['exists']);
+			});
+		}
+		else {
+			socket.write(JSON.stringify({'type': "exists", 'destination': Database, 'userid': 0 }));
+			socket.on("data", (data) => {
+				const Response = JSON.parse(data);
+				socket.destroy();
+				resolve(Response['exists']);
+			});
+		}
+	});
 }
 
 function Save(Database) {
-	const socket = net.connect(4781, "127.0.0.1");
-	socket.write(JSON.stringify({'type': "forcesave", 'destination': Database }));
-	socket.on("data", (data) => {
-		Response = JSON.parse(data);
-		socket.destroy();
-		return Response;
+	return new Promise((resolve, reject) => {
+		const socket = net.connect(4781, "127.0.0.1");
+		socket.write(JSON.stringify({'type': "forcesave", 'destination': Database }));
+		socket.on("data", (data) => {
+			Response = JSON.parse(data);
+			socket.destroy();
+			resolve(Response);
+		});
 	});
 }
 
